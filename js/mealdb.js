e376fc87631a6c9bfe2252d137166471fc840e3f
Sheikhasib <sheikhhasib037@gmail.com>
2021-08-28 22:41:32 +0600

    furr

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,10 +1,11 @@
+document.getElementById('error-message').style.display = 'none';
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // console.log(searchText);
     // clear data
-
     searchField.value = '';
+    document.getElementById('error-message').style.display = 'none';
     if(searchText == ''){
         // please write something to display
     }
@@ -13,19 +14,23 @@ const searchFood = () => {
          const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
          fetch(url)
          .then(res => res.json())
-         .then(data => displaySearchResult(data.meals))
+         .then(data => displaySearchResult(data.meals))       
+         .catch(error == displayError(error));
          // console.log(url);
-    }
-   
+    }  
 }
+
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
+};
 
 const displaySearchResult = meals => {
     // console.log(meals);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    if(meals.length == 0){
-        // show no result found
-    }
+    // if(meals.length == 0){
+    //     // show no result found
+    // }
     meals.forEach(meal => {
         // console.log(meal);
         const div = document.createElement('div');
@@ -40,7 +45,7 @@ const displaySearchResult = meals => {
         </div>
         `;
         searchResult.appendChild(div);
-    })
+    });
 }
 
 const loadMealDetail = mealId => {
---
+++ js/try.js
@@ -0,0 +1,2 @@
+const bondCode = `I am Fake James Bond.My new code is:00${7+1+2}`;
+console.log(bondCode);
